a7e7c55fd1501107b5625925dc28bcd29ab5330f
Stacy <hurrendor@gmail.com>
2016-07-08 19:11:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#TimeLiner
+##Plugin to create Timelines via the click of a button
+
+##Steps
+1. Click create
+2. Fill out the proper fields (will need form validation)
---
+++ timeliner.js
@@ -0,0 +1,13 @@
+var timeliner = {
+  year : this.year,
+  description : this.description,
+  logArticle : {
+
+  },
+
+  createLog : function (){
+
+    console.log("This creates a new day in time.")
+    document.getElementById('main').append('<article>');
+  }
+}
